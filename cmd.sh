ca41e146b796e8b7b9b5ed1beb8d92addbbc00e9
JasonZhi <369610501@qq.com>
2021-06-28 16:57:46 +0800

    centos

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,13 @@
+# centos8
+docker build -t clion/centos8-cpp-env:0.1 -f Dockerfile.centos8-cpp-env .
+docker run -d --cap-add sys_ptrace -p127.0.0.1:2222:22 --name clion_remote_env_centos8 clion/centos8-cpp-env:0.1
+ssh-keygen -f "$HOME/.ssh/known_hosts" -R "[localhost]:2222"
+# 进入shell
+docker container exec -it clion_remote_env_centos8 /bin/sh
+# 停止docker
+docker stop clion_remote_env_centos8
+
+
 # centos7
 docker build -t clion/centos7-cpp-env:0.1 -f Dockerfile.centos7-cpp-env .
 docker run -d --cap-add sys_ptrace -p127.0.0.1:2222:22 --name clion_remote_env_centos7 clion/centos7-cpp-env:0.1

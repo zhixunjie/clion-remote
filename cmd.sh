fe08aa4fc2b996b7ede5767a8176da6416155ac8
JasonZhi <369610501@qq.com>
2021-06-28 17:54:26 +0800

    docker

--- cmd.sh
+++ cmd.sh
@@ -1,16 +1,10 @@
-# centos8
-docker build -t clion/centos8-cpp-env:0.1 -f Dockerfile.centos8-cpp-env .
-docker run -d --cap-add sys_ptrace -p127.0.0.1:2222:22 --name clion_remote_env_centos8 clion/centos8-cpp-env:0.1
-ssh-keygen -f "$HOME/.ssh/known_hosts" -R "[localhost]:2222"
-# 进入shell
-docker container exec -it clion_remote_env_centos8 /bin/sh
-# 停止docker
-docker stop clion_remote_env_centos8
-
-
-# centos7
+# centos 7
+# build
 docker build -t clion/centos7-cpp-env:0.1 -f Dockerfile.centos7-cpp-env .
-docker run -d --cap-add sys_ptrace -p127.0.0.1:2222:22 --name clion_remote_env_centos7 clion/centos7-cpp-env:0.1
+# run
+docker run -d --cap-add sys_ptrace -p127.0.0.1:2222:22 \
+--name clion_remote_env_centos7 clion/centos7-cpp-env:0.1
+# ssh save
 ssh-keygen -f "$HOME/.ssh/known_hosts" -R "[localhost]:2222"
 # 进入shell
 docker container exec -it clion_remote_env_centos7 /bin/sh
@@ -18,9 +12,13 @@ docker container exec -it clion_remote_env_centos7 /bin/sh
 docker stop clion_remote_env_centos7
 
 
-# ubuntu
+# ubuntu 18.04
+# build
 docker build -t clion/remote-cpp-env:0.5 -f Dockerfile.remote-cpp-env .
-docker run -d --cap-add sys_ptrace -p127.0.0.1:2222:22 --name clion_remote_env clion/remote-cpp-env:0.5
+# run
+docker run -d --cap-add sys_ptrace -p127.0.0.1:2222:22 \
+--name clion_remote_env clion/remote-cpp-env:0.5
+# ssh save
 ssh-keygen -f "$HOME/.ssh/known_hosts" -R "[localhost]:2222"
 # 进入shell
 docker container exec -it clion_remote_env /bin/sh
